6288e9d2bbb97a4d5fc5b33c07433b3e5bff0362
MuhammadUmar7831 <mu8494759@gmail.com>
2025-03-17 18:59:13 +0500

    Project Setup

---
+++ src/models/index.ts
@@ -0,0 +1,5 @@
+export { default as User } from "./User.model";
+export { default as Chat } from "./Chat.model";
+export { default as Group } from "./Group.model";
+export { default as Message } from "./Message.model";
+export { default as FriendRequest } from "./FriendRequest.model";
---
+++ src/routes/index.ts
@@ -0,0 +1,5 @@
+export { default as userRouter } from "./user.routes";
+export { default as chatRouter } from "./chat.routes";
+export { default as groupRouter } from "./group.routes";
+export { default as messageRouter } from "./message.routes";
+export { default as friendRequestRouter } from "./friendRequest.routes";
